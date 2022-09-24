2506fe625120ebdcbdbcdd1c0de49d2d3e70445a
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-09-24 12:46:13 -0300

    Praticando

---
+++ POO/ExercicioNotaDoAluno/ExercicioNotaDoAluno/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioNotaDoAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3, total;
+
+        public override string ToString()
+        {
+            double total = Nota1 + Nota2 + Nota3;
+            double restante = 60 - total;
+
+            if (total >= 60)
+            {
+                return "Nota Final = " 
+                    + total.ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nAPROVADO";
+                
+            }
+            else
+            {
+                return "Nota Final = " 
+                    + total.ToString("F2", CultureInfo.InvariantCulture)
+                    + "\nREPROVADO"
+                    + "\nFaltaram " + restante.ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS";
+            }
+
+        }
+
+
+    }
+}
---
+++ POO/ExercicioNotaDoAluno/ExercicioNotaDoAluno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization; 
+
+namespace ExercicioNotaDoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno);
+        }
+    }
+}
