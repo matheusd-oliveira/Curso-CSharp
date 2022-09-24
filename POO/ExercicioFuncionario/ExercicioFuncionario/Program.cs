d647b68ca73e951fd0e796df1a4a4742831e5dcf
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-09-24 12:18:41 -0300

    Trabalhando com Classes e Métodos

---
+++ POO/ExercicioFuncionario/ExercicioFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando Objeto
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            
+            Console.Write("Salário Bruto : ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + func);
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
