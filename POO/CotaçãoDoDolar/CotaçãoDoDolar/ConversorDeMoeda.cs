e01d638c53a2734954adddf3054a1d85e26fe043
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-09-29 01:34:31 -0300

    Praticando com classes estáticas

---
+++ POO/CotaçãoDoDolar/CotaçãoDoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿namespace CotaçãoDoDolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quantia, double cotacao)
+        {
+            double totalDeDolar = quantia * cotacao;
+            double resultadoFinal = totalDeDolar + totalDeDolar * Iof / 100.0;
+
+            return resultadoFinal;
+        }
+    }
+}
