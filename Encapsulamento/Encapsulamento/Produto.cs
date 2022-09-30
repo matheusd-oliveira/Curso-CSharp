4ba0feb69f5b2f72903b16d10995b5c34849a4a9
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-09-30 12:09:17 -0300

    Trabalhando com Encapsulamento
    Aprendendo como encapsular. Utilizando o método Get e Set para retornar valores e atribuir valores novos.

---
+++ Encapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Produto
+    {
+        // Esses atributos não podem ser acessados por outra classe.
+        // Quando se trabalha com atributos privados, não se utiliza CamelCase por padrão
+        // Se utiliza o _letraminuscula.
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto() { }
+
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+
+        }
+
+
+        // Utilizando o Get para obter os atributos privados.
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+
+        // Utilizando o Set para setar atributos novos.
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+                _nome = nome;
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{_nome} , $ {_preco}, {_quantidade} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("TV 4K");
+          
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+        }
+    }
+}
