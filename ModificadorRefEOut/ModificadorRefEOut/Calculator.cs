211e77d3ea22ccf53a730d6a57141ff1b811d38f
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-04 21:42:24 -0300

    ref e out

--- ModificadorRefEOut/ModificadorRefEOut/Calculator.cs
+++ ModificadorRefEOut/ModificadorRefEOut/Calculator.cs
@@ -1,6 +1,16 @@
 ﻿namespace ModificadorRefEOut
 {
     class Calculator
-    {
+    {   
+        // Modificador ref
+        public static void Triple(ref int x)
+        {
+            x *= 3;
+        }
+
+        public static void Double(int origin, out int result)
+        {
+            result = origin * 2;
+        }
     }
 }
--- ModificadorRefEOut/ModificadorRefEOut/Program.cs
+++ ModificadorRefEOut/ModificadorRefEOut/Program.cs
@@ -6,7 +6,22 @@ namespace ModificadorRefEOut
     {
         static void Main(string[] args)
         {
-            
+            // Utilizando o ref
+            // o Modificador ref necessita que a variável seja iniciada.
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+            // Utilizando o OUT
+            // o Modificador OUT não necessita que a variável seja iniciada.
+
+            // AMBOS SÃO CONSIDERADOS "CODE SMELLS" (design ruim) e devem ser evitados.
+            int b = 10;
+            int dobro;
+            Calculator.Double(b, out dobro);
+            Console.WriteLine(dobro);
         }
+
+        
     }
 }
