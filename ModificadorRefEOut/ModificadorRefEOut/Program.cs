eda63ffb81ea4a9993c05f8c3ede717c4c7ac097
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-04 21:11:45 -0300

    Redirecionando diretórios

---
+++ ModificadorRefEOut/ModificadorRefEOut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModificadorRefEOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
