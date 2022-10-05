3f07e4c02b57cecc87629bbf7d1a6efc99353c47
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-05 19:30:04 -0300

    Minha atividade sem correção

---
+++ ExercicioListas01/ExercicioListas01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioListas01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"Emplyoee #{i}: ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                Employee emp = new Employee(id, name, salary);
+                employees.Add(emp);
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                if (employees[i].Id == searchId)
+                {
+                    Console.Write("Enter the percentage: ");
+                    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    employees[i].IncreaseSalary(percentage);
+                    break;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("This id does not exist!");
+                    break;
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:\n ");
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+   
+        }
+    }
+}
---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Declarando uma lista.
+            List<string> list = new List<string>();
+
+            // Adicionando elementos em uma lista.
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+
+            // Adicionando elementos em um determinado indice
+            list.Insert(2, "Marco");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            
+            // Contando quantos elementos a lista possui
+            Console.WriteLine("List count: " + list.Count);
+
+            // Como encontrar um elemento na lista utilizando lambda
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+            
+            // Como encontrar o ultimo elemento na lista de acordo com a expresssão lambda
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+            // Encontrar a primeira posição de um elemento que esteja de acordo com a expressão lambda
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position 'A': " + pos1);
+
+            // Encontrar a ultima posição de um elemento que esteja de acordo com a expressão lambda
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position 'A': " + pos2);
+
+            // Filtrar a lista de acordo com a expressão proposta.
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Filtrando a Lista: ");
+            Console.WriteLine();
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Removendo elementos da lista
+            list.Remove("Alex");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Removendo elementos da lista: ");
+            Console.WriteLine();
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Removendo todos os elementos de acordo com a expressão proposta
+            list.RemoveAll(x => x[0] == 'M');
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Removendo elementos da lista que começam com a letra M: ");
+            Console.WriteLine();
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Removendo elementos em uma posição especifica da lista 2:
+            list2.RemoveAt(1);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Removendo elemento da lista em determinado indice: ");
+            Console.WriteLine();
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
