37aa7042702f102e81c9aec7a24f6f6d83c289f1
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-17 20:23:20 -0300

    Estudando Enumerações

---
+++ POO/Enumerações/Enumerações/Program.cs
@@ -0,0 +1,31 @@
+﻿using Enumerações.Entities;
+using System;
+using Enumerações.Entities.Enums;
+
+namespace Enumerações
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1000,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            // Enum para string
+            string txt = OrderStatus.PendingPayment.ToString();
+            
+
+            // String para enum
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
