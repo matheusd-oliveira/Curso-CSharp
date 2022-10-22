059601254cdb6ad46155de065b807c20ccf12370
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-22 00:41:02 -0300

    commit

---
+++ POO/HerançaEPolimorfismo/HerançaEPolimorfismo/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace HerançaEPolimorfismo.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+
+        public SavingsAccount() { }
+
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+        : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        /// <summary>
+        /// Método para atualizar o Saldo.
+        /// </summary>
+        public void UpdateBalance() {
+
+            Balance += Balance * InterestRate;
+        }
+    }
+}
