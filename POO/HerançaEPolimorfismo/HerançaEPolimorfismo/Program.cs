815363adfa189673bddfa57d9504cc73b83b7616
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-21 23:37:13 -0300

    Create project

---
+++ POO/HerançaEPolimorfismo/HerançaEPolimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerançaEPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
