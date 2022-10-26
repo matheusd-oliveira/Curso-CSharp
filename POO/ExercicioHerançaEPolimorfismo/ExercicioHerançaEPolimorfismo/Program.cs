adcaa450abbf285454a54c7d2c6e2c397da2f408
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-26 00:37:45 -0300

    Testando conhecimentos sobre Herança e Polimorfismo com Exercicio Resolvido do Curso de Nélio Alves C# POO

--- POO/ExercicioHerançaEPolimorfismo/ExercicioHerançaEPolimorfismo/Program.cs
+++ POO/ExercicioHerançaEPolimorfismo/ExercicioHerançaEPolimorfismo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExercicioHerançaEPolimorfismo.Entities;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace ExercicioHerançaEPolimorfismo
 {
@@ -6,7 +9,44 @@ namespace ExercicioHerançaEPolimorfismo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additional));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
         }
     }
 }
