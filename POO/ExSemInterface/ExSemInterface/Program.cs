2348344b62da64b1b0b2a0e96163a145dce2a2d8
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-11-09 19:09:15 -0300

    Resolvendo exercicio utilizando Serviços
    Resolvendo exercicio , utilizando Services, porém sem interfaces.

--- POO/ExSemInterface/ExSemInterface/Program.cs
+++ POO/ExSemInterface/ExSemInterface/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExSemInterface.Entities;
+using System;
+using System.Globalization;
+using ExSemInterface.Services;
 
 namespace ExSemInterface
 {
@@ -6,7 +9,29 @@ namespace ExSemInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
