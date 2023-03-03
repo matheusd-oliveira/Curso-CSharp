ea366a3a0f7eda7ee5db7fe70d51fdca179bccfd
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-03-03 19:34:18 -0300

    ExtensionMethods
    Aprendendo sobre ExtensionMethods.

---
+++ POO/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+DateTime dt = new DateTime(2023, 03, 03, 7, 10, 45);
+
+Console.WriteLine(dt.ElapsedTime());
+
+string s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
