ea902a59dd1a0316f91b25689a3668a4516ed3dc
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-03-03 21:58:00 -0300

    Delegates
    Aprendendo sobre Delegates

---
+++ POO/DelegatesAction/DelegatesAction/Program.cs
@@ -0,0 +1,25 @@
+﻿using Delegates.Entities;
+
+List<Product> list = new();
+
+list.Add(new Product("Tv", 900.00));
+list.Add(new Product("Mouse", 50.00));
+list.Add(new Product("Tablet", 350.00));
+list.Add(new Product("HD Case", 80.90));
+
+// Utilizando o Action e passando a função como argumento.
+// Action<Product> act = UpdatePrice;
+
+Action<Product> act = p => { p.Price += p.Price * 0.1 };
+
+foreach (Product p in list)
+{
+    Console.WriteLine(p);
+}
+
+
+// Função auxiliar
+static void UpdatePrice(Product p)
+{
+    p.Price += p.Price * 0.1;
+}
