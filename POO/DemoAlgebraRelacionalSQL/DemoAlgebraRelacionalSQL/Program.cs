bb04120ed985d5146abc9d51a7489a237b4a0500
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-03-07 22:53:26 -0300

    Algebra Relacional e Sql
    Utilizando operações relacionais do SQL para manipular dados.

---
+++ POO/DemoAlgebraRelacionalSQL/DemoAlgebraRelacionalSQL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAlgebraRelacionalSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
