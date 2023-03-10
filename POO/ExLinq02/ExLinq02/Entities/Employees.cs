0628f53683e82b87d94521b744016d73a10f1868
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-03-09 21:24:32 -0300

    Exercicio de Fixação Linq Concluido

---
+++ POO/ExLinq02/ExLinq02/Entities/Employees.cs
@@ -0,0 +1,17 @@
+﻿namespace ExLinq02.Entities
+{
+    internal class Employees
+    {
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(string name, string mail, double salary)
+        {
+            Name = name;
+            Mail = mail;
+            Salary = salary;
+        }
+
+    }
+}
--- POO/ExLinq02/ExLinq02/Program.cs
+++ POO/ExLinq02/ExLinq02/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ExLinq02
+﻿using ExLinq02.Entities;
+using System.Globalization;
+namespace ExLinq02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            List<Employees> list = new();
+
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!(sr.EndOfStream))
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string mail = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employees(name, mail, salary));
+                }
+                sr.Close();
+            }
+
+            Console.Write("Enter salary: R$");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // Realizando operações Linq
+            var mails = list.Where(e => e.Salary > value).OrderBy(e => e.Mail).Select(e => e.Mail);
+            Console.WriteLine("Email of people have salary more than 2000.00:");
+            foreach (var mail in mails)
+            {
+                Console.WriteLine(mail);
+            }
+            Console.WriteLine();
+
+            var sumSalarys = list.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.Write("Sum of Employees that have a name begin with letter M is: R$");
+            Console.WriteLine(sumSalarys.ToString("F2", CultureInfo.InvariantCulture));
+            
         }
     }
 }
