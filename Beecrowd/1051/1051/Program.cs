4c51856eb6912dcc60ed9a9496c09a937a9da087
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-16 18:29:04 -0300

    Exercise 1051 complete

--- Beecrowd/1051/1051/Program.cs
+++ Beecrowd/1051/1051/Program.cs
@@ -10,33 +10,25 @@ namespace _1051
             double valorImposto = 0;
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            if (salario <= 2000.00)
+            if (salario >= 0 && salario <= 2000.00)
                 Console.WriteLine("Isento");
 
-            else if (salario <= 3000.00)
+            else if (salario > 2000.00  && salario <= 3000.00)
             {
                 imposto = 8;
-                valorImposto = (salario - 2000.00) * imposto / 100;
+                valorImposto = (salario - 2000) * imposto / 100;
                 Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             }
-
-            else if (salario == 3002.00)
-            {
-                valorImposto = (1000.00 * 8 / 100) + (2.00 * 18 / 100);
-                Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
-            }
-
             else if (salario <= 4500.00)
             {
                 imposto = 18;
-                valorImposto = (salario - 3000.00) * imposto / 100;
+                valorImposto = ((salario  - 3000 ) * imposto / 100) + (1000.00 * 8 / 100) ;
                 Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             }
-
             else 
             {
                 imposto = 28;
-                valorImposto = (salario - 4500 ) * imposto / 100;
+                valorImposto = ((salario  - 4500) * imposto / 100) + (1500 * 18 / 100) + (1000 * 8 / 100) ;
                 Console.WriteLine("R$ " + valorImposto.ToString("F2", CultureInfo.InvariantCulture));
             }
         }
