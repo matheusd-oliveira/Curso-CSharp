caf93f45516b6eece298e4ea84962b63cb2a6a8a
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-16 15:59:03 -0300

    Exercise complete

--- Beecrowd/1045/1045/Program.cs
+++ Beecrowd/1045/1045/Program.cs
@@ -1,22 +1,65 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
+
 namespace _1045
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            double A, B, C;
+            //
+            // TIPOS DE TRIANGULOS
+            // TODO: Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente,
+            // de modo que o lado A representa o maior dos 3 lados
+            // COMPLETE ***
+            float A, B, C, maior = 0;
 
-            string[] valores = Console.ReadLine().Split(' ');
+            string[] vet = Console.ReadLine().Split(' ');
 
-            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
-            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
-            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+            A = float.Parse(vet[0], CultureInfo.InvariantCulture);
+            B = float.Parse(vet[1], CultureInfo.InvariantCulture);
+            C = float.Parse(vet[2], CultureInfo.InvariantCulture);
 
-            
 
-        
+            if (A > maior)
+                maior = A;
+            if (B > maior)
+                maior = B;
+            if (C > maior)
+                maior = C;
+
+
+            if (maior == B)
+                B = A;
+
+            else if (maior == C)
+                C = A;
+
+            A = maior;
+
+            // TODO: Fazer validações para cada tipo de triângulo
+            // COMPLETE ***
+
+            if (A >= B + C)
+                Console.WriteLine("NAO FORMA TRIANGULO");
+
+            else if (Math.Pow(A, 2) == Math.Pow(B, 2) + Math.Pow(C, 2))
+                Console.WriteLine("TRIANGULO RETANGULO");
+
+            else if (Math.Pow(A, 2) > Math.Pow(B, 2) + Math.Pow(C, 2))
+                Console.WriteLine("TRIANGULO OBTUSANGULO");
+
+            else if (Math.Pow(A, 2) < Math.Pow(B, 2) + Math.Pow(C, 2))
+                Console.WriteLine("TRIANGULO ACUTANGULO");
+
+            if (A == B && B == C)
+                Console.WriteLine("TRIANGULO EQUILATERO");
+
+            else if ( (A == B && A != C) || (B == C && B != A) || (C == A && C != B))
+                Console.WriteLine("TRIANGULO ISOSCELES");
+
+
+
+
 
         }
     }
