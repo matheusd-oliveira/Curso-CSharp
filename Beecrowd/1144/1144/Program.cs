f0a6facdee2f4492df342e92fbc34c6c00a4ddb6
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-19 22:57:30 -0300

    Exercise Complete

---
+++ Beecrowd/1144/1144/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1144
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            for (int i = 1; i <= num; i++)
+            {  
+                Console.WriteLine($"{i} {i * i} {(i * i) * i}");
+                Console.WriteLine($"{i} {(i * i) + 1} {((i* i) * i) + 1}");
+            }
+        }
+    }
+}
